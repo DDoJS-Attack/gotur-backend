ac80a2e17f211fbf3dd4bf114945841b74f8eafc
Ugur <uguruy3939@gmail.com>
2018-02-17 21:41:27 +0300

    firebase initial

---
+++ helpers/firebase.js
@@ -0,0 +1,32 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../../gotur-app-firebase-adminsdk-tkymu-aeea0eb2a6.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://gotur-app.firebaseio.com',
+});
+const messaging = admin.messaging();
+// const message = {
+//   notification: {
+//     title: '$GOOG up 1.43% on the day',
+//     body: '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
+//   },
+//   topic: 'all',
+// };
+
+// messaging
+//   .send(message)
+//   .then(x => console.log(x))
+//   .catch(err => console.error(err));
+
+module.exports = {
+  sendMsg: (title, body) =>
+    messaging({
+      notification: {
+        title,
+        body,
+      },
+      topic: 'all',
+    }),
+};
