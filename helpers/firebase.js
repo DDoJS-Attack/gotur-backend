df03f8b421dae846744572af74267352185422a2
sahin <oluts@itu.edu.tr>
2018-02-17 21:39:33 +0300

    Firebase message function is added

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -7,17 +7,26 @@ admin.initializeApp({
   databaseURL: 'https://gotur-app.firebaseio.com',
 });
 const messaging = admin.messaging();
-const message = {
-  notification: {
-    title: '$GOOG up 1.43% on the day',
-    body: '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
-  },
-  topic: 'all',
+// const message = {
+//   notification: {
+//     title: '$GOOG up 1.43% on the day',
+//     body: '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
+//   },
+//   topic: 'all',
+// };
+
+// messaging
+//   .send(message)
+//   .then(x => console.log(x))
+//   .catch(err => console.error(err));
+
+module.exports = {
+  sendMsg: (title, body) =>
+    messaging({
+      notification: {
+        title,
+        body,
+      },
+      topic: 'all',
+    }),
 };
-
-messaging
-  .send(message)
-  .then(x => console.log(x))
-  .catch(err => console.error(err));
-
-module.exports = { sendMsg: msg => messaging(msg) };
